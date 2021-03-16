55ac4e645068e8e2f72e86988ac85f9b39b83c21
maxrazum <75961644+maxrazum@users.noreply.github.com>
2021-03-16 16:47:23 -0300

    feat: added API credentials

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
-/* Global Variables */
+// Base URL and API key for OpenWeatherMap API
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&appid=4949022eea8bdeddf3177bc44a5476a7&units=imperial";
 
 // Create a new date instance dynamically with JS
 let d = new Date();

902e207b828ba61db5d8f8055897529950de2246
maxrazum <75961644+maxrazum@users.noreply.github.com>
2021-03-16 16:45:51 -0300

    feat: added POST and GET routes

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+
+// Dependencies
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`server running on localhost: ${port}`);
+}
+
+// GET route
+app.get('/all', sendData);
+
+function sendData(req, res) {
+    res.send(projectData);
+}
+
+// POST route
+app.post('/add', addContent);
+
+function addContent(req, res) {
+
+    const newContent = {
+        temp: req.body.temp,
+        date: req.body.date,
+        content: req.body.content
+    };
+
+    projectData = newContent;
+    res.send(projectData);
+}
